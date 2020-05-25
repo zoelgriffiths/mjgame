6fde37c11b124ff83ba48ff6b372e27516bd1bd2
zoelgriffiths <33595456+zoelgriffiths@users.noreply.github.com>
2020-05-25 14:00:38 +0100

    Update gofish.py

--- gofish.py
+++ gofish.py
@@ -254,9 +254,13 @@ def go(player,players,players_cards,players_not_cards,all_cards,how_many_cards):
             return end_game(players_cards,players_not_cards,all_cards,how_many_cards)
         
         #Or if it one of their possible suits we accept their response of yes, giving them a copy of it if they don't already have one in their definite cards.
-        else: 
-            if suit not in players_cards[chosen_player]:
-                players_cards[chosen_player].append(all_cards[suit-1].pop(0))  
+        else:
+            if suit in possible_suits(player,players_cards,players_not_cards,all_cards):
+                if suit not in players_cards[player]:
+                    if how_many_cards[player] > len(players_cards[player]):
+                        players_cards[player].append(all_cards[suit-1].pop(0))
+                    else:
+                        return end_game(players_cards,players_not_cards,all_cards,how_many_cards)
         
     #If they say no:
     else:   
